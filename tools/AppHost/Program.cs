dee8f8b4ee5875f37d859c31f4cc438e32b74674
Renato Golia <renato.golia@tobiidynavox.com>
2024-08-12 11:29:20 +0200

    Configured Aspire

--- tools/AppHost/Program.cs
+++ tools/AppHost/Program.cs
@@ -1,3 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var db = builder.AddPostgres("pg")
+                .AddDatabase("pgdb");
+
+builder.AddProject<Projects.Api>("api")
+        .WithReference(db);
+
 builder.Build().Run();
